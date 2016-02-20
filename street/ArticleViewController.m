68ea5e83db487eecfb39d6f7ec193a9865567ac7
Graham Mosley <gmosley@seas.upenn.edu>
2016-02-20 17:30:00 -0500

    basic demo to render article in WKView

--- street/ArticleViewController.m
+++ street/ArticleViewController.m
@@ -9,6 +9,8 @@
 #import "ArticleViewController.h"
 #import <WebKit/WebKit.h>
 
+#import "Ono.h"
+
 @interface ArticleViewController () <WKNavigationDelegate>
 
 @property (nonatomic, strong) WKWebView *webview;
@@ -21,15 +23,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    NSURL *url = [NSURL URLWithString:@"http://www.34st.com"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    // Create a NSLURL that points to an article and grab the HTML
+    NSURL *articleURL = [NSURL URLWithString:@"http://www.34st.com/article/2016/02/paul-is-alive"];
+    NSString *articleHTML = [NSString stringWithContentsOfURL:articleURL encoding:NSUTF8StringEncoding error:NULL];
     
+    // Use Ono to parse the document for the article-text HTML
+    ONOXMLDocument* document = [ONOXMLDocument HTMLDocumentWithString:articleHTML encoding:NSUTF8StringEncoding error:NULL];
+    
+    // Add every article-text div to a string
+    NSMutableString *stuffToRender = [[NSMutableString alloc] init];
+    [document enumerateElementsWithXPath:@"//div[@class=\"article-text\"]" usingBlock:^(ONOXMLElement *element, NSUInteger idx, BOOL *stop) {
+        [stuffToRender appendString: [element description]];
+    }];
+    
+    // Create a webview from the parsed HTML
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     self.webview = [[WKWebView alloc] initWithFrame:self.view.frame configuration:config];
     
-    [self.webview loadRequest:request];
+    // Render the parsed HTML in a WKView
+    [self.webview loadHTMLString:stuffToRender baseURL:NULL];
     
     [self.view addSubview:self.webview];
+
+    //other helpful stuff we used
+    //NSURL *url = [NSURL URLWithString:@"http://www.34st.com"];
+    //NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    //[self.webview loadRequest:request];
     
 }
 
