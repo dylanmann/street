64d9ae2f4c494e24a0a6afc7b539f7ee37bcea93
Graham Mosley <gmosley@seas.upenn.edu>
2016-02-20 23:55:47 -0500

    basic scraping of all street articles. dylan and graham

--- street/ArticleViewController.m
+++ street/ArticleViewController.m
@@ -44,8 +44,42 @@
     [self.webview loadHTMLString:stuffToRender baseURL:NULL];
     
     [self.view addSubview:self.webview];
+    
+    // clearly this will all go into it's own class
+    
+    // all articles can be scraped from this URL
+    NSURL *articlesURL = [NSURL URLWithString:@"http://www.34st.com/section/essentials"];
+    NSString *articlesHTML = [NSString stringWithContentsOfURL:articlesURL encoding:NSUTF8StringEncoding error:NULL];
+    
+    ONOXMLDocument* articlesDoc = [ONOXMLDocument HTMLDocumentWithString:articlesHTML encoding:NSUTF8StringEncoding error:NULL];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"MM/dd/yy hh:mma";
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+    
+    // for each article, parse information
+    NSString *articlesXPath = @"//article[@class = 'standard hidden-xs']/div[@class = 'media']";
+    [articlesDoc enumerateElementsWithXPath:articlesXPath usingBlock:^(ONOXMLElement *element, NSUInteger idx, BOOL *stop) {
+        NSString *aTitle = [element firstChildWithXPath:@"./div/h4/a"].stringValue;
+        NSString *aURL = [element firstChildWithXPath:@"./div/h4/a/@href"].stringValue;
+        
+        NSString *aDateString = [element firstChildWithXPath:@"./div/aside/p/span[2]"].stringValue;
+        NSString *aAuthorWithoutTrim = [element firstChildWithXPath:@"./div/aside/p/span[1]"].stringValue;
+        
+        NSString *aAuthor = [aAuthorWithoutTrim stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        
+        NSString *aAbstract = [element firstChildWithXPath:@"./div/p[not(contains(@class, \" \"))]"].stringValue;
+        
+        NSString *aImage = [element firstChildWithXPath:@"./a/div/img/@src"].stringValue;
+        
+        NSDate *aDate = [dateFormatter dateFromString:aDateString];
+        
+        NSLog(@"\n%@\n%@\n%@\n%@\n%@\n%@\n\n", aTitle, aURL, aAuthor, aDate, aImage, aAbstract);
+        
+        
+    }];
 
-    //other helpful stuff we used
+    //other helpful stuff we used in testing
     //NSURL *url = [NSURL URLWithString:@"http://www.34st.com"];
     //NSURLRequest *request = [NSURLRequest requestWithURL:url];
     //[self.webview loadRequest:request];
