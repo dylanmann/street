91326def58c3d366232c71109ca5190712ee8409
Jenny <jennche@sas.upenn.edu>
2016-03-22 01:24:33 -0400

    swiping changes section; display article image and title

--- street/ArticlePageViewController.m
+++ street/ArticlePageViewController.m
@@ -9,12 +9,14 @@
 #import "ArticlePageViewController.h"
 #import "ArticleViewController.h"
 #import "Article.h"
+#import "ArticleData.h"
 #import <WebKit/WebKit.h>
 
 @interface ArticlePageViewController () <UIPageViewControllerDataSource>
 
 @property (nonatomic) UIPageViewController *pageViewController;
 @property (nonatomic) IBOutlet UIBarButtonItem* revealButtonItem;
+@property (weak, nonatomic) IBOutlet UIButton *screenTitle;
 
 @end
 
@@ -29,6 +31,7 @@
     self.pageViewController.dataSource = self;
     
     ArticleViewController *start = [self viewControllerAtIndex:0];
+    [_screenTitle setTitle:@"Highbrow" forState:UIControlStateNormal];
     
     [self.pageViewController setViewControllers:@[start] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
     self.pageViewController.view.frame = self.view.bounds;
@@ -36,7 +39,7 @@
     [self.pageViewController didMoveToParentViewController:self.pageViewController];
     [self.view addSubview:self.pageViewController.view];
     NSLog(@"start page view load");
-    
+
     //for menu control
     SWRevealViewController *revealViewController = self.revealViewController;
     if ( revealViewController )
@@ -56,11 +59,13 @@
 - (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
     
     ArticleViewController *avc = (ArticleViewController *)viewController;
+    
+   [self syncTitle: avc.index];
 
     if ((avc.index == 0) || (avc.index == NSNotFound)) {
         return nil;
     }
-    
+
     return [self viewControllerAtIndex:avc.index - 1];
 }
 
@@ -68,12 +73,28 @@
     
     ArticleViewController *avc = (ArticleViewController *)viewController;
     
+    [self syncTitle: avc.index];
+
     //Assumes 11 sections
     if ((avc.index == 11) || (avc.index == NSNotFound)) {
         return nil;
     }
 
     return [self viewControllerAtIndex:avc.index + 1];
+}
+
+- (void)syncTitle:(int) index {
+    
+    NSLog(@"changing title %d", index);
+    
+    if (index == 0) {
+        [_screenTitle setTitle:@"Highbrow" forState:UIControlStateNormal];
+    } else if (index == 1) {
+        [_screenTitle setTitle:@"Word on the Street" forState:UIControlStateNormal];
+    } else if (index == 2) {
+        [_screenTitle setTitle:@"Ego" forState:UIControlStateNormal];
+    }
+    
 }
 
 - (ArticleViewController *)viewControllerAtIndex:(int) index {
@@ -84,34 +105,53 @@
     
     ArticleViewController *avc = [[ArticleViewController alloc] init];
     avc.index = index;
-    
-    UIImageView *imageview = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"roundup.png"]];
-    imageview.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/2);
-    
+
     bottom += self.view.frame.size.height / 2;
     
     UILabel *label = [[UILabel alloc] init];
     label.backgroundColor = [UIColor whiteColor];
     label.textAlignment = NSTextAlignmentCenter;
-    label.font = [label.font fontWithSize:50];
+    label.font = [label.font fontWithSize:30];
     label.textColor = [UIColor blackColor];
     label.numberOfLines = 0;
-    label.frame = CGRectMake(0,self.view.frame.size.height-300,320,300);
+    label.frame = CGRectMake(0,self.view.frame.size.height/2,320,150);
     bottom += 300;
     NSURL *articleURL;
+    NSString *section;
+    
+    //TODO: Also some issues with how the button's title changes. dumb fix for now.
+    //Issue is because afterVC and beforeVC also call this method
     
     if (index == 0) {
-        label.text = @"ESSENTIALS \n The Roundup \n 2.22.16";
-        articleURL = [NSURL URLWithString:@"http://www.34st.com/article/2016/02/round-up-02-25-16"];
-    } else if (index == 1){
-        label.text = @"Quiz: Which Member of the Beatles are You?";
-        articleURL = [NSURL URLWithString:@"http://www.34st.com/article/2016/02/paul-is-alive"];
-    } else {
-        label.text = @"is this even changing/";
-        articleURL = [NSURL URLWithString:@"http://www.34st.com/article/2016/03/texts-from-last-night-spring-break-edition"];
+        section = @"highbrow";
+    } else if (index == 1) {
+        section = @"word-on-the-street";
+    } else if (index == 2) {
+        section = @"ego";
     }
-    Article *a = [[Article alloc] initWithURL:articleURL];
     
+    _screenTitle.titleLabel.numberOfLines = 1;
+    _screenTitle.titleLabel.adjustsFontSizeToFitWidth = YES;
+    _screenTitle.titleLabel.lineBreakMode = NSLineBreakByClipping;
+    
+    ArticleData* articleData = [ArticleData sharedInstance];
+    NSArray *articlesInSection = [articleData articlesForSection:section];
+    
+    //TODO: this should be refactored eventually but I was too lazy to figure out what the issue is -- something is different between initializing articles using Article.m and ArticleData.m
+    
+    Article *a = (Article *) articlesInSection[0];
+    
+    label.text = a.title;
+    NSURL *imageUrl = a.image;
+    
+    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
+    
+    UIImageView *imageview = [[UIImageView alloc] initWithImage:image];
+    imageview.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height/2);
+
+    articleURL = a.url;
+    a = [[Article alloc] initWithURL:articleURL];
+
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     WKWebView *webview = [[WKWebView alloc] initWithFrame:CGRectMake(0, bottom, self.view.frame.size.width, 500) configuration:config];
     [webview loadHTMLString:a.articleHTML baseURL:NULL];
